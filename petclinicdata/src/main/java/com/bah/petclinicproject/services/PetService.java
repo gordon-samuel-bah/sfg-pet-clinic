d4bd576bc4df3f2b1a95bc59a3deecae2b94e7c2
Sam Gordon <gordon_samuel@bah.com>
2022-06-30 10:12:00 -0500

    Refactor Service Interfaces to be like CRUD repository

--- petclinicdata/src/main/java/com/bah/petclinicproject/services/PetService.java
+++ petclinicdata/src/main/java/com/bah/petclinicproject/services/PetService.java
@@ -2,13 +2,6 @@ package com.bah.petclinicproject.services;
 
 import com.bah.petclinicproject.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save (Pet pet);
-
-    Set<Pet> findAll();
 }
