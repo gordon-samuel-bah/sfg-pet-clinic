dc0dedd03bd31a57bf3091c0e9c0fcf1b2027416
Sam Gordon <gordon_samuel@bah.com>
2022-07-08 12:00:36 -0500

    Updated the profiles for testing both data implementations

--- petclinicdata/src/main/java/com/bah/petclinicproject/services/map/OwnerMapService.java
+++ petclinicdata/src/main/java/com/bah/petclinicproject/services/map/OwnerMapService.java
@@ -5,11 +5,13 @@ import com.bah.petclinicproject.model.Pet;
 import com.bah.petclinicproject.services.OwnerService;
 import com.bah.petclinicproject.services.PetService;
 import com.bah.petclinicproject.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
