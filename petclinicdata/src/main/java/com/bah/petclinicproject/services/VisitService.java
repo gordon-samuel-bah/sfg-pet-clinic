c82ed3ad649f7cfea35e5b39d4fa8af86d90815c
Sam Gordon <gordon_samuel@bah.com>
2022-07-08 11:37:37 -0500

    Created and implemented visit map and JPA services

---
+++ petclinicdata/src/main/java/com/bah/petclinicproject/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.bah.petclinicproject.services;
+
+import com.bah.petclinicproject.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ petclinicdata/src/main/java/com/bah/petclinicproject/services/map/VisitMapService.java
@@ -0,0 +1,36 @@
+package com.bah.petclinicproject.services.map;
+
+import com.bah.petclinicproject.model.Visit;
+import com.bah.petclinicproject.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
