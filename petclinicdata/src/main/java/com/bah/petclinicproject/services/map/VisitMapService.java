7d5a2ace8316300a04eadd4c833c4a46e181c106
Sam Gordon <gordon_samuel@bah.com>
2022-07-08 11:44:08 -0500

    Updates save method to ensure owner and pet are included before save.

--- petclinicdata/src/main/java/com/bah/petclinicproject/services/map/VisitMapService.java
+++ petclinicdata/src/main/java/com/bah/petclinicproject/services/map/VisitMapService.java
@@ -20,8 +20,14 @@ public class VisitMapService extends AbstractMapService<Visit, Long> implements 
     }
 
     @Override
-    public Visit save(Visit object) {
-        return super.save(object);
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
     }
 
     @Override
